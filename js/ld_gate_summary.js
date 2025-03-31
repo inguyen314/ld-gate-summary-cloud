bed6ad302da5a77b92979cbf086ea5a097ee19fa
ivan nguyen <ivan.h.nguyen@usace.army.mil>
2025-03-31 14:55:02 -0500

    update cda to 2025.03.05

--- js/ld_gate_summary.js
+++ js/ld_gate_summary.js
@@ -39,11 +39,12 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     let setBaseUrl = null;
     if (cda === "internal") {
-        setBaseUrl = `https://wm.${office.toLowerCase()}.ds.usace.army.mil:8243/${office.toLowerCase()}-data/`;
+        setBaseUrl = `https://wm.${office.toLowerCase()}.ds.usace.army.mil/${office.toLowerCase()}-data/`;
     } else if (cda === "internal-coop") {
-        setBaseUrl = `https://wm-${office.toLowerCase()}coop.mvk.ds.usace.army.mil:8243/${office.toLowerCase()}-data/`;
+        setBaseUrl = `https://wm-${office.toLowerCase()}coop.mvk.ds.usace.army.mil/${office.toLowerCase()}-data/`;
     } else if (cda === "public") {
-        setBaseUrl = `https://cwms-data.usace.army.mil/cwms-data/`;
+        // setBaseUrl = `https://cwms-data.usace.army.mil/cwms-data/`;
+        setBaseUrl = `https://cwms.sec.usace.army.mil/cwms-data/`;
     }
     // console.log("setBaseUrl: ", setBaseUrl);
 
@@ -53,13 +54,14 @@ document.addEventListener('DOMContentLoaded', async function () {
     } else if (cda === "internal-coop") {
         setJsonFileBaseUrl = `https://wm-mvscoop.mvk.ds.usace.army.mil/`;
     } else if (cda === "public") {
-        setJsonFileBaseUrl = `https://www.mvs-wc.usace.army.mil/`;
+        // setJsonFileBaseUrl = `https://www.mvs-wc.usace.army.mil/`;
+        setBaseUrl = `https://cwms.sec.usace.army.mil/cwms-data/`;
     }
     console.log("setJsonFileBaseUrl: ", setJsonFileBaseUrl);
 
     // Define the URL to fetch location groups based on category
-    const categoryApiUrl = setBaseUrl + `location/group?office=${office}&include-assigned=false&location-category-like=${setLocationCategory}`;
-    // console.log("categoryApiUrl: ", categoryApiUrl);
+    const categoryApiUrl = setBaseUrl + `location/group?office=${office}&group-office-id=${office}&category-office-id=${office}&category-id=${setLocationCategory}`;
+    console.log("categoryApiUrl: ", categoryApiUrl);
 
     // Initialize maps to store metadata and time-series ID (TSID) data for various parameters
     const metadataMap = new Map();
